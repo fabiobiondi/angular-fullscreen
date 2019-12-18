ec0d9754e9835da4f7e60cbc428e51ba89385b26
Benoit Lavenier <benoit.lavenier@e-is.pro>
2019-12-18 16:38:47 +0100

    [fix] Fix for new webkit browser versions (no more argument when calling function  webkitRequestFullscreen())

--- src/angular-fullscreen.js
+++ src/angular-fullscreen.js
@@ -5,9 +5,6 @@
       module.factory('Fullscreen', ['$document', '$rootScope', function ($document,$rootScope) {
          var document = $document[0];
 
-         // ensure ALLOW_KEYBOARD_INPUT is available and enabled
-         var isKeyboardAvailbleOnFullScreen = (typeof Element !== 'undefined' && 'ALLOW_KEYBOARD_INPUT' in Element) && Element.ALLOW_KEYBOARD_INPUT;
-
          var emitter = $rootScope.$new();
 
          // listen event on document instead of element to avoid firefox limitation
@@ -19,20 +16,15 @@
          var serviceInstance = {
             $on: angular.bind(emitter, emitter.$on),
             all: function() {
-               serviceInstance.enable( document.documentElement );
+               serviceInstance.enable( $document.length && $document[0].body || document.documentElement);
             },
             enable: function(element) {
                if(element.requestFullScreen) {
-                  element.requestFullScreen();
+                  element.requestFullScreen(options);
                } else if(element.mozRequestFullScreen) {
                   element.mozRequestFullScreen();
                } else if(element.webkitRequestFullscreen) {
-                  // Safari temporary fix
-                  if (/Version\/[\d]{1,2}(\.[\d]{1,2}){1}(\.(\d){1,2}){0,1} Safari/.test(navigator.userAgent)) {
-                     element.webkitRequestFullscreen();
-                  } else {
-                     element.webkitRequestFullscreen(isKeyboardAvailbleOnFullScreen);
-                  }
+                  element.webkitRequestFullscreen();
                } else if (element.msRequestFullscreen) {
                   element.msRequestFullscreen();
                }
@@ -53,12 +45,12 @@
                return fullscreenElement ? true : false;
             },
             toggleAll: function(){
-                serviceInstance.isEnabled() ? serviceInstance.cancel() : serviceInstance.all();
+               serviceInstance.isEnabled() ? serviceInstance.cancel() : serviceInstance.all();
             },
             isSupported: function(){
-                var docElm = document.documentElement;
-                var requestFullscreen = docElm.requestFullScreen || docElm.mozRequestFullScreen || docElm.webkitRequestFullscreen || docElm.msRequestFullscreen;
-                return requestFullscreen ? true : false;
+               var docElm = document.documentElement;
+               var requestFullscreen = docElm.requestFullScreen || docElm.mozRequestFullScreen || docElm.webkitRequestFullscreen || docElm.msRequestFullscreen;
+               return requestFullscreen ? true : false;
             }
          };
 
